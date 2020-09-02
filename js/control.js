92b95baf6274fa50d4afb994cf37ab75f6a5c91e
Rajesh Khanna <chrajeshkhanna82798@gmail.com>
2020-09-02 22:20:16 +0530

    Update control.js

--- js/control.js
+++ js/control.js
@@ -64,8 +64,7 @@ function handleSignin(isSignedIn){
     if(path == "login.html"){
     }else{
       console.log(path)
-     setTimeout(location.replace('./login.html'), 1000);
-      
+     location.replace('./login.html');
     }
   }
 }
